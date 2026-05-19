086a9595389870f268a72b1a1dfd51200ac72010
kirill <anipixie@DESKTOP-911E237>
2026-05-19 23:45:59 +0800

    5.4 сделано

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -9,16 +9,21 @@ class SearchController extends BaseVideocardsTwigController
         $context = parent::getContext();
         // вытаскиваем значения из $_GET, т.к. может и не быть
         // то проверяем через isset()
-        $type = isset($_GET['type']) ? $_GET['type'] : '';
+        $type = isset($_GET['type']) ? $_GET['type'] : 'all';
         $title = isset($_GET['title']) ? $_GET['title'] : '';
+        $info = isset($_GET['info']) ? $_GET['info'] : '';
 
+        // значения обратно в контекст (чтобы фильтры сохранялись в полях)
+        $context['type'] = $type;
+        $context['title'] = $title;
+        $context['info'] = $info;
 
-        
         $sql = <<<EOL
 SELECT id, title
 FROM videocards_object
 WHERE (:title = '' OR title LIKE CONCAT('%', :title, '%'))
-AND (type = :type)
+AND (:info = '' OR info LIKE CONCAT('%', :info, '%'))
+AND (:type = 'all' OR type = :type)
 EOL;
         // WHERE (:title = '' OR title LIKE CONCAT('%', :title, '%')) тут проверка либо что название 
         // указали пустым либо если не пустое то сверяем с названиями в бд на частичное совпадение, 
@@ -28,6 +33,7 @@ EOL;
 
         $query->bindValue("title", $title);
         $query->bindValue("type", $type);
+        $query->bindValue("info", $info);
         $query->execute();
 
         $context['objects'] = $query->fetchAll();
