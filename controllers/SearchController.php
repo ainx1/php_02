a66d0668d5895d0b69a2a158d6507956ca497dd8
kirill <anipixie@DESKTOP-911E237>
2026-05-19 22:01:12 +0800

    5.4 не работает по методичке, разбираемся

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -7,6 +7,28 @@ class SearchController extends BaseVideocardsTwigController
     public function getContext(): array
     {
         $context = parent::getContext();
+        // вытаскиваем значения из $_GET, т.к. может и не быть
+        // то проверяем через isset()
+        $type = isset($_GET['type']) ? $_GET['type'] : '';
+        $title = isset($_GET['title']) ? $_GET['title'] : '';
+
+        $sql = <<<EOL
+SELECT id, title
+FROM videocards_object
+WHERE (:title = '' OR title LIKE CONCAT('%', :title, '%'))
+AND (type = :type)
+EOL;
+        // WHERE (:title = '' OR title LIKE CONCAT('%', :title, '%')) тут проверка либо что название 
+        // указали пустым либо если не пустое то сверяем с названиями в бд на частичное совпадение, 
+        // для этого переданное название оборачиваем в проценты
+
+        $query = $this->pdo->prepare($sql);
+
+        $query->bindValue("title", $title);
+        $query->bindValue("type", $type);
+        $query->execute();
+
+        $context['objects'] = $query->fetchAll();
 
         return $context;
     }
