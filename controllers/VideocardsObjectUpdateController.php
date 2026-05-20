ee53ebfab40cb1699a8cc7bdc79802fd6cab2738
kirill <anipixie@DESKTOP-911E237>
2026-05-20 11:45:35 +0800

    5.7 работает, но надо доделать чутка

---
+++ controllers/VideocardsObjectUpdateController.php
@@ -0,0 +1,71 @@
+   <?php
+    require_once "BaseVideocardsTwigController.php";
+
+    class VideocardsObjectUpdateController extends BaseVideocardsTwigController
+    {
+        public $template = "videocards_object_create.twig";
+
+        public function get(array $context) // добавили параметр (5.7 редактирование)
+        {
+            // echo $_SERVER['REQUEST_METHOD'];
+            $id = $this->params['id']; // получили id из параметров
+
+            $sql = <<<EOL
+SELECT * FROM videocards_object WHERE id = :id
+EOL;
+            $query = $this->pdo->prepare($sql);
+            $query->bindValue("id", $id);
+            $query->execute();
+
+            $data = $query->fetch();
+            $context['object'] = $data; // передаем данные в контекст
+
+            parent::get($context); // пробросили параметр
+
+        }
+        // добавил
+        public function post(array $context)
+        {
+            $id = $this->params['id']; // получили id из параметров
+            // получаем значения полей с формы
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+            $type_id = $_POST['type_id']; // изменил на type_id, тк теперь передаем id типа а не его название
+            $info = $_POST['info'];
+
+            // вытащил значения из $_FILES
+            $tmp_name = $_FILES['image']['tmp_name'];
+            $name =  $_FILES['image']['name'];
+
+            // используем функцию которая проверяет
+            // что файл действительно был загружен через POST запрос
+            // и если это так, то переносит его в указанное во втором аргументе место
+            move_uploaded_file($tmp_name, "../public/media/$name");
+            $image_url = "/media/$name"; // формируем ссылку без адреса сервера
+
+            // создаем текст запрос
+            $sql = <<<EOL
+UPDATE videocards_object 
+SET title = :title, description = :description, type_id = :type_id, info = :info, image = :image_url 
+WHERE id = :id
+EOL;
+
+            // подготавливаем запрос к БД
+            $query = $this->pdo->prepare($sql);
+            // привязываем параметры
+            $query->bindValue("title", $title);
+            $query->bindValue("description", $description);
+            $query->bindValue("type_id", $type_id);
+            $query->bindValue("info", $info);
+            $query->bindValue("image_url", $image_url); // подвязываем значение ссылки к переменной  image_url
+            $query->bindValue("id", $id); // подвязываем значение id к переменной  id
+
+            // выполняем запрос
+            $query->execute();
+
+            $context['message'] = 'Вы успешно обновили объект';
+            $context['object'] = $_POST; // Чтобы данные в форме остались после нажатия кнопки
+
+            $this->get($context);
+        }
+    }
--- public/index.php
+++ public/index.php
@@ -4,12 +4,13 @@ require_once '../vendor/autoload.php';
 require_once '../framework/autoload.php';
 
 require_once "../controllers/MainController.php"; // добавим в самом верху ссылку на наш контроллер
-require_once "../controllers/ObjectController.php"; // добавил 
+require_once "../controllers/ObjectController.php"; // добавил 5.3
 require_once "../controllers/SearchController.php"; // добавил 5.4
 
 require_once "../controllers/VideocardsObjectCreateController.php"; // добавил 5.5
 require_once "../controllers/TypeCreateController.php";
 require_once "../controllers/VideocardsObjectDeleteController.php"; // добавил 5.6
+require_once "../controllers/VideocardsObjectUpdateController.php"; // добавил 5.7
 require_once "../controllers/Controller404.php";
 
 
@@ -38,5 +39,6 @@ $router->add("/type/create", TypeCreateController::class);
 $router->add("/", MainController::class);
 $router->add("/videocards_object/(?P<id>\d+)", ObjectController::class);
 $router->add("/videocards_object/delete", VideocardsObjectDeleteController::class);
+$router->add("/videocards_object/(?P<id>\d+)/edit", VideocardsObjectUpdateController::class);
 
 $router->get_or_default(Controller404::class);
